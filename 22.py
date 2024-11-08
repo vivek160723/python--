269cdad5539893778b1301d818885b5288e94cdd
vivek <vivekchaudhary160723@gmail.com>
2024-11-08 12:53:11 +0530

    first commit

--- 22.py
+++ 22.py
@@ -1,7 +1,9 @@
-#patterns
 N=5
-for i in range(N):
-      for j in range(i+1):
-          print(" *",end=" ")
-      print()
-
+for i in range (N):
+    for j in range(1,N-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*", end=" ")
+    for l in range(i+1):
+        print("*", end=" ")
+    print()
